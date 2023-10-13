7c65bf127e35d19aed1f06bb36c7265ede7e3b73
Shelly Anissa <shellyannissa29@gmail.com>
2023-10-13 11:19:02 +0530

    Completion of both frontend and backend

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+//Components
+import InputTodo from "./components/InputTodos";
+import ListTodos from "./components/ListTodos";
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+        <ListTodos />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
